99887b2841133e67e57013ccdcbb517c13cc9489
siddique mursal <siddiquemursal@gmail.com>
2021-06-27 18:15:33 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript & React.js Bootcamp</h1>
+      <p>Basic Web Dev React.js Bootcamp Starting from 1st July 2021</p>
+    </div>
+  );
+}
+
+export default Info;
